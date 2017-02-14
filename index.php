5c579e65031fcbd330a26621fe61600a26212bdc
Solman Vaisman <solman28@gmail.com>
2017-02-13 22:03:52 -0500

    cambios

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <meta property="og:site_name" content="Escuela de seguros apecose" />
     <meta property="og:title" content="Ramos Técnicos de Ingeniería: seguros, reaseguros, siniestros y ajustes" />
     <meta property="og:description" content="El Diplomado Ramos Técnicos de Ingeniería: seguros, reaseguros, siniestros y ajustes elevará los conocimientos de los participantes al nivel de especialistas en riesgos y seguros de ingeniería para operaciones industriales y proyectos. Su contenido y casuística se basan en las exigencias profesionales del mercado. Por su amplitud y profundidad es de lejos una excelente inversión en actualización y capacitación." />
-    <meta property="og:image" content="postlinkedin.png" />
+    <meta property="og:image" content="https://test-app-php.mybluemix.net/postlinkedin.png" />
     <meta property="og:url" content="https://lnkd.in/eTqfwkn" />
     <meta property="og:type" content="blog"/>
 </head>
