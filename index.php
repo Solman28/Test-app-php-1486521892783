640fc0860e74b66924a33b76131fb65229cd9803
Solman Vaisman <solman28@gmail.com>
2017-02-13 21:34:44 -0500

    cambios

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta property="og:title" content="Ramos Técnicos de Ingeniería: seguros, reaseguros, siniestros y ajustes"/>
     <meta property="og:description" content="El Diplomado Ramos Técnicos de Ingeniería: seguros, reaseguros, siniestros y ajustes elevará los conocimientos de los participantes al nivel de especialistas en riesgos y seguros de ingeniería para operaciones industriales y proyectos. Su contenido y casuística se basan en las exigencias profesionales del mercado. Por su amplitud y profundidad es de lejos una excelente inversión en actualización y capacitación."/>
     <meta property="og:image" content="postlinkedin.png">
-    <meta property="og:url" content="http://escueladesegurosapecose.edu.pe/wp-content/uploads/2017/02/Riesgos-de-ingenieri%CC%81a-III-1.pdf">
+    <meta property="og:url" content="//escueladesegurosapecose.edu.pe/wp-content/uploads/2017/02/Riesgos-de-ingenieri%CC%81a-III-1.pdf">
     <meta property="og:type" content="blog"/>
 </head>
 <body>
